c669fcd32063c8efae2f70834c468fa404793a6e
bapjingan <bagasadityaputra.bap@gmail.com>
2021-04-17 16:06:14 +0700

    tidying up html php

--- php/login.php
+++ php/login.php
@@ -1,53 +1,77 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="./assets/icon.png" type="image/x-icon">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">   
+    <!-- Boostrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>BeeHub</title>
+</head>
+<body>
+    <header>
+        <div id="header-left">
+            <img src="./assets/menu icon.png" alt="">
+        </div>
+        <div id="header-center">
+            <img src="./assets/icon.png" alt="">
+            <h1 class="orange">BeeHub</h1>
+        </div>
+        <div id="header-right">
+            <div class="link">
+                <a href="">Login</a>
+            </div>
+            <div class="link">
+                <a href="">Register</a>
+            </div>
+        </div>
+    </header>
 
-$username = filter_input(INPUT_POST, 'username');
-$email = filter_input(INPUT_POST, 'email');
-$name = filter_input(INPUT_POST, 'name');
-$password = filter_input(INPUT_POST, 'password');
+<!-- Register Form -->
 
-if (!empty($username)){
-    if (!empty($email)){
-        if (!empty($name)){
-            if (!empty($password)){
-                $host = "localhost";
-                $dbusername = "root";
-                $dbpassword = "";
-                $dbname = "beehub";
-        
-                //create connection
-                $conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
-                if (mysqli_connect_error()){
-                    die('Connect Error ('.mysqli_connect_errno().')' . mysqli_connect_error());
-                }
-                else{
-                    $sql = "INSERT INTO register (username, email, name, password) values ('$username', '$email', '$name', '$password')";
-                    if ($conn->query($sql)){
-                        echo "New record is inserted sucessfully";
-                    }
-                    else{
-                        echo "Error: ". $sql . "<br>". $conn->error;
-                    }
-                    $conn->close();
-                }
-            }
-            else{
-                echo "Password should not be empty";
-                die();
-            }
-        }
-        else{
-            echo "Name should not be empty";
-            die();
-        }
-    }
-    else{
-        echo "Email should not be empty";
-        die();
-    }
-}
-else{
-    echo "Username should not be empty";
-    die();
-}
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 offset-md-4 form-div"></div>
+            <form action="register.php" method="post">
+                <h3 class="text-center">Register</h3>
 
-?>
+                <!-- <div class="alert alert-danger">
+                    <li>Username Required</li>
+                </div> -->
+
+                <div class="from-group">
+                    <label for="username">Username</label>
+                    <input type="text" name="username" class="form-control from-control-lg">
+                </div>
+
+                <div class="from-group">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" class="form-control from-control-lg">
+                </div>
+
+                <div class="from-group">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" class="form-control from-control-lg">
+                </div>
+
+                <div class="from-group">
+                    <label for="passwordconf">Confirm Password</label>
+                    <input type="password" name="passwordconf" class="form-control from-control-lg">
+                </div>
+
+                <div class="form-group">
+                    <button type="submit" name="signup-btn" class="btn btn-primary btn-block btn-lg">Sign Up</button>
+                </div>
+                <p class="text-center">Already a member?<a href="login.php">Sign In</a></p>
+            </form>
+            </div>
+        </div>
+    </div>
+
+
+
+</body>
+</html>
